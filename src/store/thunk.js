5a8704f49d4367ba9626b1aac8214879a4f84595
Apoorve Verma <vapoorve@gmail.com>
2023-05-24 19:48:42 +0530

    API Updated

--- src/store/thunk.js
+++ src/store/thunk.js
@@ -1,11 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-const LOCATION_ORIGIN = window.location.origin;
-const PROPERTIES_API_ENDPOINT = LOCATION_ORIGIN + "/src/properties.json";
+// const LOCATION_ORIGIN = window.location.origin;
+// const PROPERTIES_API_ENDPOINT = LOCATION_ORIGIN + "/src/properties.json";
 export const fetchAllPropertiesList = createAsyncThunk(
   "properties/fetchAllPropertiesList",
   async () => {
     try {
-      const response = await fetch(PROPERTIES_API_ENDPOINT);
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Apoorve8055/Crowd-Fund-Castle/master/src/properties.json"
+      );
       const data = await response.json();
       return data?.properties;
     } catch (err) {
